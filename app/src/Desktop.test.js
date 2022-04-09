7d0381fec7039f3656d7d0e076afa068739cfb04
Jeff Qiu <zhenfeng.qiu@tufts.edu>
2022-04-09 17:18:16 -0400

    Remove test files

--- app/src/Desktop.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-it('Test test', () => {
-  render(<App />);
-  expect(screen.getByText('Hi')).toBeInTheDocument();
-});
